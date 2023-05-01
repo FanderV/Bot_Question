59c55c8419b376c26e306a5fd1b64bb15ea61a6e
FanderV <113282846+FanderV@users.noreply.github.com>
2023-05-01 13:44:37 +0300

    1

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import requests
 import json
 import datetime
 import time
+import configparser
 
-#Задаем токен
-token = telebot.TeleBot("token")
+# конфиг
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+token = telebot.TeleBot(config['telegram']['token'])
+id_chat = config['telegram']['chat_id']
+
+hour = int(config['telegram']['hour'])
+min = int(config['telegram']['min'])
 
 def is_workday(date):
-    link = "http://xmlcalendar.ru/data/ru/" + str(date.year) + "/calendar.json" #ссылка
-    dan = requests.get(link) #получаем данные
-    inf = json.loads(dan.text) #преобразуем в json
-    for month in inf["months"]: #проверка на рабочий день
+    link = "http://xmlcalendar.ru/data/ru/" + str(date.year) + "/calendar.json"  # ссылка
+    dan = requests.get(link)  # получаем данные
+    inf = json.loads(dan.text)  # преобразуем в json
+    for month in inf["months"]:  # проверка на рабочий день
         if month["month"] == date.month:
             days = month["days"].split(",")
             for day in days:
-                if day.endswith("+") or day.endswith("*"): #проверяем на лишние символы * and +
-                    day = day.strip("+*") #удаляем лишние символы
+                if day.endswith("+") or day.endswith("*"):  # проверяем на лишние символы * and +
+                    day = day.strip("+*")  # удаляем лишние символы
                 if str(date.day) == day:
                     return False
             return True
@@ -24,24 +32,24 @@ def is_workday(date):
 
 
 @token.message_handler(commands=['question'])
-def send_question(message): #функция отправки опроса
+def send_question(message):  # функция отправки опроса
     if is_workday(datetime.datetime.now().date()):
-        token.send_poll(chat_id='id',
+        token.send_poll(chat_id=id_chat,
                         question='Где Вы сегодня работаете?',
                         options=['Офис', 'Дом', 'Объект'],
                         is_anonymous=False)
 
 
 while True:
-    now = datetime.datetime.now() #проверяем время
-    if now.hour == 8 and now.minute == 0 and is_workday(now.date()):
+    now = datetime.datetime.now()  # проверяем время
+    if now.hour == hour and now.minute == min and is_workday(now.date()):
         try:
-            token.send_poll(chat_id='id',
+            token.send_poll(chat_id=id_chat,
                             question='Где Вы сегодня работаете?',
                             options=['Офис', 'Дом', 'Объект'],
                             is_anonymous=False)
-            time.sleep(60) #ожидание в минуту перед следующей проверкой времени
+            time.sleep(60)  # ожидание в минуту перед следующей проверкой времени
         except Exception as e:
             print("Произошла ошибка:", e)
             continue
-        time.sleep(60) #ожидание в минуту
+        time.sleep(60)  # ожидание в минуту
