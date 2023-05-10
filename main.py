2f722f796fad4c8731fcb85bbca87af603c9e341
FanderV <113282846+FanderV@users.noreply.github.com>
2023-05-10 18:08:41 +0300

    1

--- main.py
+++ main.py
@@ -5,37 +5,49 @@ import datetime
 import time
 import configparser
 
-# конфиг
+# Конфигурация
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-#токены тг
+# Инициализация токена и идентификатора чата из конфигурационного файла
 token = telebot.TeleBot(config['telegram']['token'])
 id_chat = config['telegram']['chat_id']
-#время отправки
+
+# Получение часа и минуты опроса из конфигурационного файла
 hour = int(config['telegram']['hour'])
 minute = int(config['telegram']['min'])
 
-last_sent_time = None #отслеживает время последней отправки опроса
-calendar_data = None # хранит данные о календаре с информацией о рабочих и нерабочих днях
-this_year = datetime.datetime.now().year #текущий год
+# Переменные для хранения времени последней отправки опроса, данных о календаре и текущего года
+last_time = None
+calendarnaya_data = None
+this_year = datetime.datetime.now().year
+
+
+def load_calendar_data(year):
+    # Функция для загрузки данных о календаре с сайта
+    link = "http://xmlcalendar.ru/data/ru/" + str(year) + "/calendar.json"
+    try:
+        answer = requests.get(link)
+        answer.raise_for_status()
+        return json.loads(answer.text)
+    except requests.RequestException as e:
+        print("Произошла ошибка при получении данных о календаре:", e)
+        return None
 
 
 def is_workday(date):
-    global calendar_data, this_year
+    # Функция для проверки, является ли день рабочим
+    global calendarnaya_data, this_year
 
-    if calendar_data is None or date.year != this_year:
-        link = "http://xmlcalendar.ru/data/ru/" + str(date.year) + "/calendar.json"
-        try:
-            answer = requests.get(link)
-            answer.raise_for_status()
-            calendar_data = json.loads(answer.text)
-            this_year = date.year
-        except requests.RequestException as e:
-            print("Произошла ошибка при получении данных о календаре:", e)
+    # Проверка, требуется ли загрузка данных о календаре для текущего года
+    if calendarnaya_data is None or date.year != this_year:
+        calendarnaya_data = load_calendar_data(date.year)
+        if calendarnaya_data is None:
             return True
 
-    for month in calendar_data["months"]:
+        this_year = date.year
+
+    for month in calendarnaya_data["months"]:
         if month["month"] == date.month:
             days = month["days"].split(",")
             for day in days:
@@ -48,17 +60,18 @@ def is_workday(date):
 
 @token.message_handler(commands=['question'])
 def send_question(message):
-    global last_sent_time
+    # Обработчик команды для отправки опроса
+    global last_time
 
     now = datetime.datetime.now()
-    if is_workday(now.date()) and (last_sent_time is None or now.date() > last_sent_time.date()):
+    if is_workday(now.date()) and (last_time is None or now.date() > last_time.date()):
         token.send_poll(
             chat_id=id_chat,
             question='Где Вы сегодня работаете?',
             options=['Офис', 'Дом', 'Объект'],
             is_anonymous=False
         )
-        last_sent_time = now
+        last_time = now
 
 
 while True:
@@ -69,4 +82,10 @@ while True:
         except Exception as e:
             print("Произошла ошибка при отправке опроса:", e)
             continue
+
+    # Проверка смены года для обновления календарных данных
+    if now.year != this_year:
+        this_year = now.year
+        calendarnaya_data = load_calendar_data(this_year)
+
     time.sleep(30)
